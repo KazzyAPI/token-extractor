4422cf2084796f59db381fba5a3c09d8b75ba1a3
KazzyDev <60765113+KazzyAPI@users.noreply.github.com>
2021-06-29 14:46:31 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const tokens = fs.readFileSync('./extract.txt', 'utf-8');
+const split = tokens.split('\n');
+let token = '';
+split.forEach(element => {
+    //console.log(element);
+    if(element.startsWith('N') || element.startsWith('M') ||element.startsWith('O')) {
+        if(token != element) {
+            if(element.length < 50) return;
+            console.log(element);
+            fs.appendFile('Extracted strings.txt', `${element}\n`, async err => {
+                if(err) throw err;
+            })
+            token = element;
+        }
+    }
+});
